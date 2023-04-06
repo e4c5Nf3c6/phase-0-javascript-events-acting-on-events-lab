4e7a413aa04b9822422eccdf0e09689d3c0f43db
Maria Jenkins <macjenkin@gmail.com>
2023-04-06 11:14:32 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const dodger = document.getElementById("dodger");
-const game = document.getElementById("game");
 
 function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
@@ -10,22 +9,19 @@ function moveDodgerLeft() {
     }
 }
 
-document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") {
-        moveDodgerLeft();
-    }
-});
-
 function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
+
     if (left < 360) {
         dodger.style.left = `${left + 1}px`;
     }
 }
 
-document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowRight") {
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
         moveDodgerRight();
     }
 });
